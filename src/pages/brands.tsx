552b97ab4bb58ddadf847966c5886f0bf59d08f8
Shlomo Y <shlomo@techtio.io>
2025-11-11 09:27:11 +0200

    demo sidebar

---
+++ src/pages/brands.tsx
@@ -0,0 +1,9 @@
+const Brands = () => {
+  return (
+    <div className="home-page">
+      <h1>Brands</h1>
+    </div>
+  );
+};
+
+export default Brands;
---
+++ src/pages/campaigns.tsx
@@ -0,0 +1,9 @@
+const Campaigns = () => {
+  return (
+    <div className="home-page">
+      <h1>Campaigns</h1>
+    </div>
+  );
+};
+
+export default Campaigns;
